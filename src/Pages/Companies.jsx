4737c6194a161c412cc286008ddbd3e811e682f8
melendez-dev <sams20030818@gmail.com>
2022-08-25 22:24:06 -0500

    feature: finish thirdSection company page

--- src/Pages/Companies.jsx
+++ src/Pages/Companies.jsx
@@ -1,12 +1,14 @@
 import Header from "../Components/Common/ModuleCompanies/Header";
 import FirstSection from "../Components/Common/ModuleCompanies/FirstSection";
 import SecondSection from "../Components/Common/ModuleCompanies/SecondSection";
+import ThirdSection from "../Components/Common/ModuleCompanies/ThridSection";
 export default function Companies({ toggle }) {
   return (
     <>
       <Header toggle={toggle} />
       <FirstSection />
       <SecondSection />
+      <ThirdSection />
     </>
   );
 }
